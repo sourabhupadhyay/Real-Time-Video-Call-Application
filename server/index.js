4773913c04a08622c554f041139673e443459740
Sourabh upadhyay <upadhyaysourabh12@gmail.com>
2024-05-28 14:46:56 +0530

    Real time videochat

---
+++ server/index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const { Server } = require("socket.io");
+const http = require("http");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: true,
+});
+
+const emailToSocketIdMap = new Map();
+const socketidToEmailMap = new Map();
+
+io.on("connection", (socket) => {
+  console.log(`Socket Connected`, socket.id);
+  socket.on("room:join", (data) => {
+    const { email, room } = data;
+    emailToSocketIdMap.set(email, socket.id);
+    socketidToEmailMap.set(socket.id, email);
+    io.to(room).emit("user:joined", { email, id: socket.id });
+    socket.join(room);
+    io.to(socket.id).emit("room:join", data);
+  });
+
+  socket.on("user:call", ({ to, offer }) => {
+    io.to(to).emit("incomming:call", { from: socket.id, offer });
+  });
+
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    console.log("peer:nego:needed", offer);
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    console.log("peer:nego:done", ans);
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
+});
+
+// Serve static files from the "public" directory
+const publicPath = path.join(__dirname, "public");
+app.use(express.static(publicPath));
+
+// Define the port for production
+const PORT = process.env.PORT || 8000;
+
+// Start listening on the defined port
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
